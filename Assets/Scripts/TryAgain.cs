7059f35f26413b74cb80c5a546a1ae49058aefd4
Whike <49380278+whitefangalive@users.noreply.github.com>
2024-02-23 12:59:07 -0600

    Fixed Don't destroy on load and added text to phone

--- Assets/Scripts/TryAgain.cs
+++ Assets/Scripts/TryAgain.cs
@@ -9,6 +9,7 @@ public class TryAgain : MonoBehaviour
     public void clicking()
     {
         Debug.Log("Clicked Try again button");
+        Destroy(GameObject.Find("disableDDOL"));
         SteamVR_LoadLevel.Begin(SceneManager.GetActiveScene().name);
     }
 }
